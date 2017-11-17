37aafb62f2ff10078147d08a270608a2aed05fde
Jeppe Pihl <jpihl08@gmail.com>
2017-11-17 14:05:50 +0100

    working on it

--- source/src/main/java/com/steinwurf/score/source/Version.java
+++ source/src/main/java/com/steinwurf/score/source/Version.java
@@ -1,4 +1,13 @@
 package com.steinwurf.score.source;
+/*-
+ * Copyright (c) 2017 Steinwurf ApS
+ * All Rights Reserved
+ *
+ * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF STEINWURF
+ * The copyright notice above does not evidence any
+ * actual or intended publication of such source code.
+ */
+
 
 @SuppressWarnings("JniMissingFunction")
 public class Version
